4133919470bb72daf22cbc046ac7acca032e2b03
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-07-10 18:50:00 -0500

    Working on functionality to integrate past, present, and future classes

--- script.js
+++ script.js
@@ -14,12 +14,29 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  var currentHour = dayjs().format('H');
+  console.log(currentHour);
+
+  var timeBlock = $('.time-block');
+  var containerEl = $('.container');
+  console.log(timeBlock);
+  containerEl.find(timeBlock);
+  
+  // for (each time block) {
+  //   if (hour id < H) {
+  //     add the past class
+  //   } else if (hour id > H) {
+  //     add the future class
+  //   } else {
+  //     add the present class
+  //   }
+  // }
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 
   // Displays today's date as Monday, July 10, 2023 (example)
   var today = dayjs();
